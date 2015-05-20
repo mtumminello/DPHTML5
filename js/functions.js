6e14976ad8ca8b66078030384adae4c5ca86bef1
19950 <michael_tumminello@gensler.com>
2015-05-20 13:57:40 -0500

    layout updates

--- js/functions.js
+++ js/functions.js
@@ -1,14 +1,15 @@
 function navClick(id){
-			resetCSS();
+	resetCSS();
 			var mId = "m"+id;
-			$("#"+mId).css("display","block");	
+			$(".marqueeMain").css("display","none");
+			$("#"+mId).css("display","block");
 			$("#"+id).css("color","white");
 			$("#"+id+" span").css("visibility","visible");
 			$("#"+id+" span").css("color","red");
 }
 
 function resetCSS(){
-			$(".columns").css("display","none");
+			$(".marquee").css("display","none");
 			$("#sidebar li").css("color","rgba(179,221,242,1)");
 			$("#sidebar li span").css("visibility","hidden");
 			$("#sidebar li").css(":hover","hidden");
